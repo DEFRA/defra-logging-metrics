c9e43b17e44b1cd4f9e316f7a2633f6be87d8f14
Joe Lyons <joseph.lyons@condatis.com>
2024-01-23 16:42:24 +0000

    282436 - Comply with Defra standards - add JS linting

--- sample/sample.js
+++ sample/sample.js
@@ -1,37 +1,37 @@
 import 'dotenv/config'
 import applicationInsights from 'applicationinsights'
-applicationInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start()
-
 import MetricsService from '@defra/logging-metrics/metrics.js'
+
+applicationInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start()
 
 // Connection string will be sourced from APPLICATIONINSIGHTS_CONNECTION_STRING environment variable if not supplied here.
 // A local .env file (not tracked by git) can be used locally to define environment variables.
-let config = {
-    //connectionString: "", 
-    samplingPercentage: 100
+const config = {
+  // connectionString: "",
+  samplingPercentage: 100
 }
 
-let metric = {
-    name: "defraloggingmetrics-sampleMetric",
-    properties: { sampleDimension: "sampleDimensionValue" }
+const metric = {
+  name: 'defraloggingmetrics-sampleMetric',
+  properties: { sampleDimension: 'sampleDimensionValue' }
 }
 
-let metricsService = new MetricsService()
+const metricsService = new MetricsService()
 
-let myFunction = () => {
-    return "A value"
+const myFunction = () => {
+  return 'A value'
 }
 
-let myFunctionWithError = () => {
-    throw new Error("An error")
+const myFunctionWithError = () => {
+  throw new Error('An error')
 }
 
-let delayedFunction = (delay) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve('A value');
-        }, delay);
-    });    
+const delayedFunction = (delay) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('A value')
+    }, delay)
+  })
 }
 
 // execute
@@ -44,23 +44,23 @@ console.log(`Took ${res.duration} seconds for ${res.result}`)
 
 // executeAsync
 await metricsService.executeAsync(() => delayedFunction(500), metric, config)
-.then(({ result, duration }) => {
+  .then(({ result, duration }) => {
     console.log(`Took ${duration} seconds for ${result}`)
-})
+  })
 
 // executeAllAsync
 await metricsService.executeAllAsync([() => delayedFunction(500), () => delayedFunction(800)], metric, config)
-.then(({ results, durations }) => {
+  .then(({ results, durations }) => {
     results.forEach((result, i) => {
-        console.log(`Took ${durations[i]} seconds for ${result}`)
-    })    
-})
+      console.log(`Took ${durations[i]} seconds for ${result}`)
+    })
+  })
 
 // execute with error
 try {
-    metricsService.execute(() => myFunctionWithError(), metric, config)
+  metricsService.execute(() => myFunctionWithError(), metric, config)
 } catch (error) {
-    console.log(`Executed function threw error: ${error.message}`)
+  console.log(`Executed function threw error: ${error.message}`)
 }
 
 metricsService.flushClient()
